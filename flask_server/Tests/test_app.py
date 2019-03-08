4242e0c3a6b7eae2833a83410b3652d93d1f6cef
Zac Chu <zac.chu@gmail.com>
2019-03-08 14:41:56 -0500

    added test configuration

--- flask_server/Tests/test_app.py
+++ flask_server/Tests/test_app.py
@@ -19,8 +19,14 @@ def app():
 def client(app):
     return app.test_client()
 
-def test_configs():
+def test_dev_config():
     assert config.DevConfig.DEBUG == True
     assert config.DevConfig.TESTING == False
     assert config.DevConfig.DATABASE == "real_cert.json"
     assert config.DevConfig.SECRET_KEY == "dev"
+
+def test_test_config():
+    assert config.TestConfig.DEBUG == False
+    assert config.TestConfig.TESTING == True
+    assert config.TestConfig.DATABASE == "test_cert.json"
+    assert config.TestConfig.SECRET_KEY == "dev"
--- flask_server/config.py
+++ flask_server/config.py
@@ -4,3 +4,9 @@ class DevConfig():
     DATABASE = "real_cert.json"
     SECRET_KEY = "dev"
 
+class TestConfig():
+    DEBUG = False
+    TESTING = True
+    DATABASE = "test_cert.json"
+    SECRET_KEY = "dev"
+
