8f0c26993033ddf9a565c40d7f069c021be66930
Zac Chu <zac.chu@gmail.com>
2019-03-08 14:46:29 -0500

    configs all added

--- flask_server/Tests/test_app.py
+++ flask_server/Tests/test_app.py
@@ -30,3 +30,9 @@ def test_test_config():
     assert config.TestConfig.TESTING == True
     assert config.TestConfig.DATABASE == "test_cert.json"
     assert config.TestConfig.SECRET_KEY == "dev"
+
+def test_prod_config():
+    assert config.ProdConfig.DEBUG == False
+    assert config.ProdConfig.TESTING == False
+    assert config.ProdConfig.DATABASE == "real_cert.json"
+    assert config.ProdConfig.SECRET_KEY == ""
--- flask_server/config.py
+++ flask_server/config.py
@@ -1,12 +1,16 @@
-class DevConfig():
-    DEBUG = True
+class BaseConfig():
+    DEBUG = False
     TESTING = False
     DATABASE = "real_cert.json"
     SECRET_KEY = "dev"
 
-class TestConfig():
-    DEBUG = False
+class DevConfig(BaseConfig):
+    DEBUG = True
+
+class TestConfig(BaseConfig):
     TESTING = True
     DATABASE = "test_cert.json"
-    SECRET_KEY = "dev"
+
+class ProdConfig(BaseConfig):
+    SECRET_KEY = ""
 
